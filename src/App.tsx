e1620229508e1157e6031c8210740e7b9f63a94d
Rainbooboo <omoneferuemu@gmail.com>
2025-03-28 09:04:51 +0100

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 // import { useState } from 'react'
 
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Login from "./Auth/Login"
+
 function App() {
 
   return (
     <>
-     
+      <Router >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/notebook/:id" element={<NotebookView />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
     </>
   )
 }
