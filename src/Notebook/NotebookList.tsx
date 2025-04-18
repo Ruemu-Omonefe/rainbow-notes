122c71963825ffb77f972daad023c0b147907717
Rainbooboo <omoneferuemu@gmail.com>
2025-04-18 10:09:15 +0100

    images

--- src/Notebook/NotebookList.tsx
+++ src/Notebook/NotebookList.tsx
@@ -1,13 +1,26 @@
 import SwapVertIcon from '@mui/icons-material/SwapVert';
+import style from "../styes/notebookList.module.scss"
+import img from "../assets/book.png"
 
 function NotebookList() {
+  const noteBookCover = {
+    id: '1',
+    title: 'Math Notes',
+    coverStyle: 'lined',
+  }
 
     return (
       <>
         <div className="flex items-center p-5">
-        <p className="absolute left-1/2 transform -translate-x-1/2">Notebook</p>
-        <SwapVertIcon className="ml-auto" />
-      </div>
+          <p className="absolute left-1/2 transform -translate-x-1/2">Notebook</p>
+          <SwapVertIcon className="ml-auto" />
+        </div>
+
+        <div className="p-5">
+        <div className={`${style.notebookCover} ${style.leather}`}>
+          wdoweo
+        </div>
+        </div>
 
 
       </>
