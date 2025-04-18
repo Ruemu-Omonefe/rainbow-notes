83e0a07dc00a8c4c6376dba4667380fa1159f65f
Rainbooboo <omoneferuemu@gmail.com>
2025-04-18 12:30:52 +0100

    part

--- src/Notebook/NotebookList.tsx
+++ src/Notebook/NotebookList.tsx
@@ -1,13 +1,23 @@
 import SwapVertIcon from '@mui/icons-material/SwapVert';
 import style from "../styes/notebookList.module.scss"
-import img from "../assets/book.png"
+import img from "../assets/cover.png"
+import img2 from "../assets/book-cover3.png"
+import { useState } from 'react';
 
 function NotebookList() {
-  const noteBookCover = {
-    id: '1',
-    title: 'Math Notes',
-    coverStyle: 'lined',
-  }
+  const noteBookCover =[
+    {
+      id: 1,
+      title: 'Pastel Pink',
+      image: img2,
+    },
+    {
+      id: 2,
+      title: 'Leather Gold',
+      image: img,
+    },
+  ]
+  // const [title, setTitle] = useState('')
 
     return (
       <>
@@ -16,13 +26,13 @@ function NotebookList() {
           <SwapVertIcon className="ml-auto" />
         </div>
 
-        <div className="p-5">
-        <div className={`${style.notebookCover} ${style.leather}`}>
-          wdoweo
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4">
+          {noteBookCover.map(notebook => (
+            <div key={notebook.id} className={style.cover}
+              style={{ backgroundImage: `url(${notebook.image})`}}>
+            </div>
+          ))}
         </div>
-        </div>
-
-
       </>
     )
   }
